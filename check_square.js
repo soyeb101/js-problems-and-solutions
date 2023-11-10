39698d13ab04184af107e2da25c87a51c63ba84c
soyeb101 <soyeb10191@gmail.com>
2023-11-10 19:32:27 +0600

    optimized solutions of check_square

--- check_square.js
+++ check_square.js
@@ -1,23 +1,34 @@
-// Checking Square in another array
-// arr=[1,2,3,4], arr2=[1,5,8,11]
-
 function checkSquare(arr1,arr2) {
-    for(let i=0;i<arr1.length;i++){
-    let isSquare = false;
-    for(let j=0;i<arr1.length;i++) {
-        if(arr1[i] * arr1[i] === arr2[j]){
-            isSquare = true;
+    let map1={};
+    let map2={};
+    for(item of arr1) {
+        map1[item] = (map2[item] || 0)  + 1;
+    }
+    for(item1 of arr2) {
+        map2[item1] = (map2[item1] || 0)  + 1;
+    }
+
+    for(let key in map1) {
+        if(!map2[key * key]){
+            return false;
         }
-        if(j === arr2.length - 1){
-            if(!isSquare){
-                return false;
-            }
+        if(map1[key]!== map2[key * key]){
+            return false;
         }
     }
-}
+
     return true;
+
 }
 
-console.log(checkSquare([1,2,3,4], [1,9,4,15]));
+console.log(checkSquare([1,2,3,4], [1,9,4,16]));
 
-// time complexity O(n^2)
+
+// time complexity O(n) linear time complexity
+
+
+
+
+
+
+
