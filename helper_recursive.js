a777ff2d8b4aeb2217a07633a20543c81382145e
soyeb101 <soyeb10191@gmail.com>
2023-11-11 12:19:36 +0600

    find odd

---
+++ helper_recursive.js
@@ -0,0 +1,21 @@
+// Helper Recursive Function
+// [1,2,3,4,5,6,7,8,9,10]
+// [1,2,5,7,9] -> Output
+// number%2!==0
+
+function findOdd(arr) {
+    let result = [];
+    function helperRecursive(inputArray) {
+        if(inputArray.length === 0){
+            return;
+        }if(inputArray[0] %2!==0){
+            result.push(inputArray[0]);
+        }
+        helperRecursive(inputArray.slice(1))
+    }
+    helperRecursive(arr);
+    return result;
+}
+
+const res =findOdd([1,2,3,4,5,6,7,8,9,10]);
+console.log(res);
